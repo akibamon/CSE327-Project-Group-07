5569b3ee986621d85bd841217bfded3c5e780ae3
hasanulislam <80346869+hasanulislam@users.noreply.github.com>
2021-05-08 23:44:59 +0600

    Test My Class

---
+++ MembershipOfficerModel.java
@@ -0,0 +1,17 @@
+class MembershipOfficerModel {
+ 
+    public void MemberShipModel(){
+        System.out.println("Test My class");
+    }
+
+}
+public class Main {
+ 
+
+  public static void main(String[] args) {
+    MembershipOfficerModel test = new MembershipOfficerModel();
+    MemberShipModel();
+    
+ }
+  
+}
